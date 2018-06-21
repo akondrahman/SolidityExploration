6e5db47dc6d881c75a0ea8fcd5a491c406ca13e3
Akond Rahman <Akond.Ashfaque.Ur.Rahman@ibm.com>
2018-06-21 10:45:41 -0400

    code for gas miner

---
+++ main_gas_miner.py
@@ -0,0 +1,11 @@
+'''
+Mine gas cost estimate 
+Akond Rahman 
+June 21, 2018 
+'''
+
+
+if __name__=='__main__':
+   the_file = '/Users/akond.rahman/Documents/Personal/misc/solidity_output/prior/GITHUB_V4_MEENELY.csv'
+   dt_out_fil = '/Users/akond.rahman/Documents/Personal/misc/solidity_output/DATE.GITHUB.V4.csv'    
+   org = '/Users/akond.rahman/Documents/Personal/smart_contracts_research/data_sources/V4/'   
