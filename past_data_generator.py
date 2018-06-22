34e00b49d083e68914396b77002c858142b367a0
Akond Rahman <Akond.Ashfaque.Ur.Rahman@ibm.com>
2018-06-22 15:54:49 -0400

    mining past data

--- past_data_generator.py
+++ past_data_generator.py
@@ -9,8 +9,11 @@ import pandas as pd
 import numpy as np
 import itertools
 from datetime import datetime
+import subprocess
 
 def genValidMonths(s_mon, e_mon, y_l, m_l):
+    valid_months = []
+
     s_year = int(s_mon.split('-')[0])
     s_mont = int(s_mon.split('-')[-1])
 
@@ -23,13 +26,43 @@ def genValidMonths(s_mon, e_mon, y_l, m_l):
     all_mon = list(itertools.product(y_l, m_l)) 
     for mont_ in all_mon:
         cand_mont = datetime(mont_[0], mont_[1], 1)
-        print mont_, cand_mont, start_, end_
-        print '*'*50
+        # print cand_mont, sta_, end_
         if ( (cand_mont >= sta_) and (cand_mont <= end_) ):
-           print 'asi mama'
+           valid_months.append(cand_mont)
+        #    print 'asi mama'
+        #    print '*'*50
+    return valid_months
 
 
+def resetTheDir(src, des, val_mon):
+    repo_name = src.split('/')[-2]
 
+    year_ = val_mon.split('-')[0] 
+    mont_ = val_mon.split('-')[-1]
+ 
+    folder2create = des + repo_name + '-' + year_ + '-' + mont_ + '/'
+ 
+    print '='*50
+    print folder2create
+    print '-'*25
+    if((os.path.exists(folder2create))==False):
+        print src, folder2create, year_, mont_
+        
+        # try:
+        #   shutil.copytree(src, folder2create)
+        #   '''
+        #   now  do a reset
+        #   '''
+        #   cdCommand            = "cd " + folder2create + " ; "
+        #   date2reset           = year_ + '-' + mont_ + '-' + '28'  ## 28 th of the month
+        #   commitCommand        = "git checkout -f `git rev-list -n 1 --before='"+ date2reset +"' master`"
+        #   command2Run          = cdCommand + commitCommand
+        #   try:
+        #     subprocess.check_output(['bash','-c', command2Run])
+        #   except subprocess.CalledProcessError as subpro_err:
+        #     print 'Subprocess error:', subpro_err
+        # except shutil.Error as err_:
+        #     print 'Directory not copied, error:', err_
 
 def generatePastData(file_inp, y_l, m_l):
     df_ = pd.read_csv(file_inp) 
@@ -44,6 +77,11 @@ def generatePastData(file_inp, y_l, m_l):
         sta_mon = start_month_list[0]  ## start month list is already sorted 
         end_mon = end_month_list[-1]  ## start month list is already sorted 
         val_mon_lis = genValidMonths(sta_mon, end_mon, y_l, m_l)
+        for val_mon in val_mon_lis:
+            srcDir = repo_
+            desDir = '/Users/akond.rahman/Documents/Personal/misc/sol_time_data/'
+            resetTheDir(srcDir, desDir, val_mon)
+           
 
 if __name__=='__main__':
    dt_fi = '/Users/akond.rahman/Documents/Personal/misc/solidity_output/DATE.GITHUB.ALL.LOCKED.csv'
