3d97d0bd15292d79a4f322509d261f6e863246ac
Akond Rahman <Akond.Ashfaque.Ur.Rahman@ibm.com>
2018-06-25 10:33:44 -0400

    web scrapping

--- html_scrapper.py
+++ html_scrapper.py
@@ -8,6 +8,14 @@ from lxml import html
 import requests 
 
 if __name__=='__main__':
-    url_ = 'http://econpy.pythonanywhere.com/ex/001.html'
+    # url_ = 'http://econpy.pythonanywhere.com/ex/001.html'
+    url_ = 'https://etherscan.io/address/0x34f0d846c766874413938994da32360cf0e4350d#code'    
+
     page = requests.get(url_)
     tree = html.fromstring(page.content)  
+
+    source = tree.xpath('//pre[@class="js-sourcecopyarea"]/text()')
+    print source
+
+    # buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+    # print buyers 
