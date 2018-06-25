ace1a4ede240072a322a9f224f0e2fa5707e08a1
Akond Rahman <Akond.Ashfaque.Ur.Rahman@ibm.com>
2018-06-25 11:14:06 -0400

    web scrapping

--- html_scrapper.py
+++ html_scrapper.py
@@ -3,19 +3,31 @@ Akond Rahman
 June 25, 2018 
 Monday 
 HTML Scrapping 
+reff: http://docs.python-guide.org/en/latest/scenarios/scrape/
 '''
 from lxml import html
 import requests 
 
+def getLinks(link_file):
+    web_links = []
+    with open(link_file, 'rU') as _fil:
+         file_str = _fil.read()
+         web_links = file_str.split('\n')
+         web_links = [x_ for x_ in if x_!='\n']
+    return web_links   
+
 if __name__=='__main__':
-    # url_ = 'http://econpy.pythonanywhere.com/ex/001.html'
-    url_ = 'https://etherscan.io/address/0x34f0d846c766874413938994da32360cf0e4350d#code'    
+    eth_link_file = '/Users/akond.rahman/Documents/Personal/smart_contracts_research/100_filtered_contracts_list.txt'
+    # url_ = 'https://etherscan.io/address/0x34f0d846c766874413938994da32360cf0e4350d#code'    
+    # page = requests.get(url_)
+    # tree = html.fromstring(page.content)  
+    # source = tree.xpath('//pre[@class="js-sourcecopyarea"]/text()')[0] ## output is stored in a list 
+    # ### one contract can have multiple SOL files as source 
+    # # print source
+    # txCountStr  = tree.xpath('//span[@title="Normal Transactions"]/text()')[0] ## output is stored in a list 
+    # txCount     = int(txCountStr.split(' ')[0])
+    # # print txCount
 
-    page = requests.get(url_)
-    tree = html.fromstring(page.content)  
+    links = getLinks(eth_link_file)
+    print links
 
-    source = tree.xpath('//pre[@class="js-sourcecopyarea"]/text()')
-    print source
-
-    # buyers = tree.xpath('//div[@title="buyer-name"]/text()')
-    # print buyers 
