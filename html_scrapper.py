8ba62f3ca6f4faceda02b12bf9f0588023bc62a9
Akond Rahman <Akond.Ashfaque.Ur.Rahman@ibm.com>
2018-06-25 11:29:52 -0400

    web scrapping

--- html_scrapper.py
+++ html_scrapper.py
@@ -13,21 +13,30 @@ def getLinks(link_file):
     with open(link_file, 'rU') as _fil:
          file_str = _fil.read()
          web_links = file_str.split('\n')
-         web_links = [x_ for x_ in if x_!='\n']
+         web_links = [x_ for x_ in web_links if len(x_) > 0]
     return web_links   
+
+def getContentOfWebPage(link_str):
+    page = requests.get(link_str)
+    tree = html.fromstring(page.content)  
+    source = tree.xpath('//pre[@class="js-sourcecopyarea"]/text()')[0] ## output is stored in a list 
+    ### one contract can have multiple SOL files as source 
+    # print source
+    txCountStr  = tree.xpath('//span[@title="Normal Transactions"]/text()')[0] ## output is stored in a list 
+    txCount     = int(txCountStr.split(' ')[0])
+    print txCount
 
 if __name__=='__main__':
     eth_link_file = '/Users/akond.rahman/Documents/Personal/smart_contracts_research/100_filtered_contracts_list.txt'
     # url_ = 'https://etherscan.io/address/0x34f0d846c766874413938994da32360cf0e4350d#code'    
-    # page = requests.get(url_)
-    # tree = html.fromstring(page.content)  
-    # source = tree.xpath('//pre[@class="js-sourcecopyarea"]/text()')[0] ## output is stored in a list 
-    # ### one contract can have multiple SOL files as source 
-    # # print source
-    # txCountStr  = tree.xpath('//span[@title="Normal Transactions"]/text()')[0] ## output is stored in a list 
-    # txCount     = int(txCountStr.split(' ')[0])
-    # # print txCount
+
 
     links = getLinks(eth_link_file)
-    print links
+    # print links
+    count = 0 
+    for web_lin in links:
+       getContentOfWebPage(web_lin)
+       count += 1 
+       print '{} processed, {} more to go'.format(count, len(links) - count)
+       print '*'*25
 
