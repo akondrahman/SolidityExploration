d1ab9e43da287b086e3cedf01d1ed3249da28e87
Akond Rahman <Akond.Ashfaque.Ur.Rahman@ibm.com>
2018-07-08 21:42:52 -0400

    prep code for time analysis

--- time_anal.py
+++ time_anal.py
@@ -10,9 +10,15 @@ def generateCSV(file_para):
     full_df = pd.read_csv(file_para)
     all_months = full_df['CREATION_DATE'].tolist()
     uni_months = np.unique(all_months)
-    print uni_months
+    #print uni_months
+    for month in uni_months:
+        sol_files = full_df[full_df['CREATION_DATE']==month]['file_'].tolist()
+        uni_sol_files = np.unique(sol_files)
+        print month
+        print uni_sol_files 
+        print '='*50
 
 
 if __name__=='__main__':
-   the_fil = ''
+   the_fil = '/Users/akond.rahman/Documents/Personal/misc/solidity_output/FINAL_PROCESS_GITHUB.csv'
    generateCSV(the_fil)
