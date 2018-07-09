e37bd246d9813c9289ec03fdc52ee5ec6ed735b3
Akond Rahman <Akond.Ashfaque.Ur.Rahman@ibm.com>
2018-07-08 21:49:09 -0400

    prep code for time analysis

--- time_anal.py
+++ time_anal.py
@@ -5,20 +5,31 @@ Script to prepapre data for temporal analysis
 '''
 import pandas as pd
 import numpy as np
+import os 
 
-def generateCSV(file_para):
+def dumpContentIntoFile(strP, fileP):
+    fileToWrite = open( fileP, 'w')
+    fileToWrite.write(strP )
+    fileToWrite.close()
+    return str(os.stat(fileP).st_size) 
+
+def generateCSV(file_para, out_file):
     full_df = pd.read_csv(file_para)
     all_months = full_df['CREATION_DATE'].tolist()
     uni_months = np.unique(all_months)
     #print uni_months
+    mon_lis  = []
+    str_ = ''
     for month in uni_months:
         sol_files = full_df[full_df['CREATION_DATE']==month]['file_'].tolist()
         uni_sol_files = np.unique(sol_files)
-        print month
-        print uni_sol_files 
-        print '='*50
-
+        mon_lis.append((month, len(uni_sol_files)))
+    for val_ in mon_lis:
+        str_ = str_ + str(val_[0]) + ',' + str(val_[1]) + '\n'
+    str_ = 'MONTH,COUNT' + '\n' + str_
+    dumpContentIntoFile(str_, out_file)
 
 if __name__=='__main__':
    the_fil = '/Users/akond.rahman/Documents/Personal/misc/solidity_output/FINAL_PROCESS_GITHUB.csv'
-   generateCSV(the_fil)
+   output  = '/Users/akond.rahman/Documents/Personal/misc/solidity_output/FINAL_MONTH_WISE_COUNT.csv'
+   generateCSV(the_fil, output)
