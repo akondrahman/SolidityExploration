594a77ebb1558824cbd4c3559f2a6a1a9bbae653
Akond Rahman <akond.rahman.buet@gmail.com>
2019-11-20 00:01:08 -0600

    ieee software work

--- feat_impo.py
+++ feat_impo.py
@@ -1,13 +1,10 @@
 
 '''
 Akond Rahman
-June 25, 2017
-Monday
+Tuesday 
+Nov 19, 2019 
 Feature importance for Solidity Metrics using RF
 '''
-from sklearn.feature_selection import RFE
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
@@ -41,25 +38,28 @@ def calcFeatureImp(feature_vec, label_vec, feature_names_param, output_file, rep
     feat_imp_vector=theRndForestModel.feature_importances_
     #print feat_imp_vector
 
-    for ind_ in xrange(repeat):
-        for imp_vec_index in xrange(len(feat_imp_vector)):
+    for ind_ in range(repeat):
+        for imp_vec_index in range(len(feat_imp_vector)):
             feat_imp_val = round(feat_imp_vector[imp_vec_index], 5)
             str2write = str2write +  str(feat_imp_val) + ','
-            print 'Metric:{}, score:{}'.format(feature_names_param[imp_vec_index], feat_imp_val)
-            print '-'*25
+            print('Metric:{}, score:{}'.format(feature_names_param[imp_vec_index], feat_imp_val) )
+            print('-'*25 )
         str2write = str2write + '\n'
-        print '='*50
+        print('='*50 )
     str2write = header_str + '\n' + str2write
     output_status= dumpContentIntoFile(str2write, output_file)
-    print 'Dumped the RF FEATURE IMPORTANCE file of {} bytes'.format(output_status)
+    print('Dumped the RF FEATURE IMPORTANCE file of {} bytes'.format(output_status) )
 
 
 if __name__=='__main__':
 #    ds_file_name       = '/Users/akond/Documents/AkondOneDrive/OneDrive/solidity-nier2018/results/V1/FINAL_PROCESS_GITHUB_FEAT_IMP.csv'
 #    output_file_param  = '/Users/akond/Documents/AkondOneDrive/OneDrive/solidity-nier2018/results/V1/FEAT_IMP_LOCKED.csv'
 
-   ds_file_name       = '/Users/akond/Documents/AkondOneDrive/OneDrive/solidity-nier2018/results/V2/FINAL_PROCESS_GITHUB_PROCESSED.csv'
-   output_file_param  = '/Users/akond/Documents/AkondOneDrive/OneDrive/solidity-nier2018/results/V2/FEAT_IMP_LOCKED.csv'
+  #  ds_file_name       = '/Users/akond/Documents/AkondOneDrive/OneDrive/solidity-nier2018/results/V2/FINAL_PROCESS_GITHUB_PROCESSED.csv'
+  #  output_file_param  = '/Users/akond/Documents/AkondOneDrive/OneDrive/solidity-nier2018/results/V2/FEAT_IMP_LOCKED.csv'
+
+   ds_file_name       = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/ncsu_research/solidity-nier2018/results/V3/FEAT_IMP_METRICS.csv'
+   output_file_param  = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/ncsu_research/solidity-nier2018/results/V3/FEAT_IMP_LOCKED.csv'
 
    full_ds=readDataset(ds_file_name)
    full_rows, full_cols = np.shape(full_ds)
@@ -70,4 +70,4 @@ if __name__=='__main__':
    non_defected_file_count = len([x_ for x_ in all_labels if x_==0.0])
    feature_names = getColumnNames(ds_file_name, 2, feature_cols)
    calcFeatureImp(all_features, all_labels, feature_names, output_file_param)
-   print '='*100
+   print('='*100 )
